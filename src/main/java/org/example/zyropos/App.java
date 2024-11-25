3d66ae804bd8bfcb6e7665b20699471d5ba24448
Aneeq Zafar <zafaraay@gmail.com>
2024-11-25 20:44:43 +0500

    Worked on Splash Screen(1)

--- src/main/java/org/example/zyropos/App.java
+++ src/main/java/org/example/zyropos/App.java
@@ -2,17 +2,21 @@ package org.example.zyropos;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Splash.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        Parent root=FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/Splash.fxml")));
+        Scene scene=new Scene(root);
+        String css= Objects.requireNonNull(this.getClass().getResource("css/Splash.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setTitle("Splash");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/zyropos/SplashController.java
+++ src/main/java/org/example/zyropos/SplashController.java
@@ -1,14 +1,61 @@
 package org.example.zyropos;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
 
-public class SplashController {
-    @FXML
-    private Label welcomeText;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class SplashController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX App!");
+    private ProgressIndicator myProgress;
+    @FXML
+    private Label myLabel;
+    @FXML
+    private Label lblHeading;
+
+    public SplashController() {
+        System.out.println("Constructor Called");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        increaseProgress();
+    }
+
+    public void increaseProgress(){
+        Thread thread=new Thread(()->{
+            double progress=0.0;
+
+            while(progress<1.0){
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                progress+=Math.random()*0.1;
+                if(progress>1.0){
+                    progress=1.0;
+                }
+
+                double finalProgress=progress;
+                Platform.runLater(()->{
+                    //progressIndicator.setProgress(finalProgress);
+                    myLabel.setText((int) (finalProgress*100)+"%");
+
+                    if(finalProgress*100==100){
+//                        myProgress.setDisable(true);
+                        myLabel.setText("Loading Done");
+                    }
+                });
+            }
+        });
+
+        thread.setDaemon(true);
+        thread.start();
     }
 }
