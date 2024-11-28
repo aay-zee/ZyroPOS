474607b71f978dcb8622d4e99d22bed7d8d6524c
Aneeq Zafar <zafaraay@gmail.com>
2024-11-28 23:49:47 +0500

    Worked on the General Login Screen

---
+++ src/main/java/org/example/zyropos/GeneralLoginController.java
@@ -0,0 +1,21 @@
+package org.example.zyropos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class GeneralLoginController {
+    @FXML
+    private Label lblRoleSelect;
+    @FXML
+    private Button btnSA;
+    @FXML
+    private Button btnAB;
+    @FXML
+    private Button btnC;
+    @FXML
+    private Button btnDO;
+
+    
+
+}
--- src/main/java/org/example/zyropos/SplashController.java
+++ src/main/java/org/example/zyropos/SplashController.java
@@ -1,15 +1,30 @@
 package org.example.zyropos;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
+import javafx.util.Duration;
+import org.kordamp.bootstrapfx.BootstrapFX;
+
 public class SplashController implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private ProgressIndicator myProgress;
     @FXML
@@ -50,6 +65,15 @@ public class SplashController implements Initializable {
                     if(finalProgress*100==100){
 //                        myProgress.setDisable(true);
                         myLabel.setText("Loading Done");
+                        PauseTransition pause=new PauseTransition(Duration.seconds(0.5));
+                        pause.setOnFinished(event -> {
+                            try {
+                                switchToGeneralLogin();
+                            } catch (IOException e) {
+                                System.out.println(e.getMessage());
+                            }
+                        });
+                        pause.play();
                     }
                 });
             }
@@ -57,5 +81,17 @@ public class SplashController implements Initializable {
 
         thread.setDaemon(true);
         thread.start();
+    }
+
+    public void switchToGeneralLogin() throws IOException {
+        root= FXMLLoader.load(Objects.requireNonNull(getClass().getResource("fxml/GeneralLogin.fxml")));
+        stage=(Stage)myProgress.getScene().getWindow();
+        scene=new Scene(root);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+        String css= Objects.requireNonNull(this.getClass().getResource("css/gLogin.css")).toExternalForm();
+        scene.getStylesheets().add(css);
+        stage.setScene(scene);
+        stage.sizeToScene();
+        stage.show();
     }
 }
